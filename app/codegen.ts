db55ceb5531e321170357c9a728533672e4302dd
Optimisuke <naosuke0219@gmail.com>
2023-08-18 16:06:15 +0900

    add codegen

---
+++ app/codegen.ts
@@ -0,0 +1,26 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+const apiKey = process.env.API_KEY || "invalid-api-key";
+
+const config: CodegenConfig = {
+  schema: [
+    {
+      "https://miracatu.stepzen.net/api/todos/__graphql": {
+        headers: {
+          Authorization: `Apikey ${apiKey}`,
+        },
+      },
+    },
+  ],
+  documents: ["./**/*.graphql"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./src/gql/": {
+      preset: "client",
+    },
+  },
+};
+
+export default config;
