269b628688acd354b127e972e0c01f73cef6d265
Optimisuke <naosuke0219@gmail.com>
2023-08-18 16:06:47 +0900

    add generated sources

---
+++ app/src/gql/graphql.ts
@@ -0,0 +1,145 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** `DateTime` scalar type represents a date and time. DateTime is serialized as an RFC 3339 quoted string */
+  DateTime: { input: any; output: any; }
+};
+
+/**
+ * Mutation root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `mutation`.
+ *
+ * If an operation is a `mutation`, the result of the operation is the result of executing the mutation’s
+ * top level selection set on the `Mutation` root object type. This selection set is executed serially.
+ *
+ * It is expected that the top level fields in a `mutation` operation perform side‐effects on backend data systems.
+ * Serial execution of the provided mutations ensures against race conditions during these side‐effects.
+ */
+export type Mutation = {
+  __typename?: 'Mutation';
+  /**  Mutations for type 'Todos'  */
+  deleteTodos?: Maybe<Todos>;
+  insertTodos?: Maybe<Todos>;
+  updateTodos?: Maybe<Todos>;
+};
+
+
+/**
+ * Mutation root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `mutation`.
+ *
+ * If an operation is a `mutation`, the result of the operation is the result of executing the mutation’s
+ * top level selection set on the `Mutation` root object type. This selection set is executed serially.
+ *
+ * It is expected that the top level fields in a `mutation` operation perform side‐effects on backend data systems.
+ * Serial execution of the provided mutations ensures against race conditions during these side‐effects.
+ */
+export type MutationDeleteTodosArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+/**
+ * Mutation root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `mutation`.
+ *
+ * If an operation is a `mutation`, the result of the operation is the result of executing the mutation’s
+ * top level selection set on the `Mutation` root object type. This selection set is executed serially.
+ *
+ * It is expected that the top level fields in a `mutation` operation perform side‐effects on backend data systems.
+ * Serial execution of the provided mutations ensures against race conditions during these side‐effects.
+ */
+export type MutationInsertTodosArgs = {
+  completed?: InputMaybe<Scalars['Boolean']['input']>;
+  created_at?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['ID']['input'];
+  title: Scalars['String']['input'];
+  updated_at?: InputMaybe<Scalars['DateTime']['input']>;
+};
+
+
+/**
+ * Mutation root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `mutation`.
+ *
+ * If an operation is a `mutation`, the result of the operation is the result of executing the mutation’s
+ * top level selection set on the `Mutation` root object type. This selection set is executed serially.
+ *
+ * It is expected that the top level fields in a `mutation` operation perform side‐effects on backend data systems.
+ * Serial execution of the provided mutations ensures against race conditions during these side‐effects.
+ */
+export type MutationUpdateTodosArgs = {
+  completed?: InputMaybe<Scalars['Boolean']['input']>;
+  created_at?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['ID']['input'];
+  title?: InputMaybe<Scalars['String']['input']>;
+  updated_at?: InputMaybe<Scalars['DateTime']['input']>;
+};
+
+/**
+ * Query root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `query`.
+ *
+ * If an operation is a `query`, the result of the operation is the result of
+ * executing the query’s top level selection set with the `Query` root object type.
+ */
+export type Query = {
+  __typename?: 'Query';
+  /**  Queries for type 'Todos'  */
+  todos?: Maybe<Todos>;
+  todosList?: Maybe<Array<Maybe<Todos>>>;
+  todosPaginatedList?: Maybe<Array<Maybe<Todos>>>;
+};
+
+
+/**
+ * Query root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `query`.
+ *
+ * If an operation is a `query`, the result of the operation is the result of
+ * executing the query’s top level selection set with the `Query` root object type.
+ */
+export type QueryTodosArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+/**
+ * Query root object type.
+ *
+ * Contains fields that are available at the top level of a GraphQL `query`.
+ *
+ * If an operation is a `query`, the result of the operation is the result of
+ * executing the query’s top level selection set with the `Query` root object type.
+ */
+export type QueryTodosPaginatedListArgs = {
+  after?: InputMaybe<Scalars['Int']['input']>;
+  first?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Todos = {
+  __typename?: 'Todos';
+  completed?: Maybe<Scalars['Boolean']['output']>;
+  created_at?: Maybe<Scalars['DateTime']['output']>;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+  updated_at?: Maybe<Scalars['DateTime']['output']>;
+};
